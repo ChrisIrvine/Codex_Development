9ec5d7d35fdd31296cc10d903bbded311a986231
Christopher Irvine <christopher@irvinegroup.eu>
2018-01-08 19:10:37 +0000

    First proper commit

--- server.js
+++ server.js
@@ -28,3 +28,9 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', '/build/index.html'));
 });
+
+app.get('/api/test', (req, res) => {
+   res.send({ok:true}); 
+});
+
+module.exports = { app }
--- server.test.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test';
-
-const chai = require('chai');
-const chaiHTTP = require('chai-http');
-const server = require('./server');
-const app = server.app;
-const should = chai.should();
-
-chai.use(chaiHTTP);
